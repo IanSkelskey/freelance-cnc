819b218e28de68f21009ac7bc8ee15b9eaff7037
Ian Skelskey <ianskelskey@gmail.com>
2023-09-26 00:35:37 -0400

    Update origin on test object

--- src/three/Cube.js
+++ src/three/Cube.js
@@ -1,6 +1,5 @@
-// Cube.js
-import React, { useRef } from 'react';
-import { Canvas, useLoader, useFrame } from '@react-three/fiber';
+import React, { useRef, useEffect } from 'react';
+import { Canvas, useLoader, useFrame, useThree } from '@react-three/fiber';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { OrbitControls } from '@react-three/drei';
 import { Mesh, MeshStandardMaterial } from 'three';
@@ -13,6 +12,7 @@ const Model = ({ animate }) => {
     if (animate) {
       meshRef.current.rotation.x += 0.01;
       meshRef.current.rotation.y += 0.01;
+      meshRef.current.rotation.z += 0.01;
     }
   });
 
@@ -20,25 +20,26 @@ const Model = ({ animate }) => {
   obj.traverse((child) => {
     if (child instanceof Mesh) {
       child.material = new MeshStandardMaterial({
-        color: '#aaaaaa',  // Stainless steel color
+        color: '#ffffff',  // Stainless steel color
         metalness: 0.8,    // High metalness for a metallic look
         roughness: 0.2,    // Low roughness for a shiny surface
       });
+      child.scale.set(0.02, 0.02, 0.02);
     }
   });
 
   return <primitive object={obj} ref={meshRef} />;
 };
 
-
-
 export default function CubeCanvas({ animate }) {
   return (
     <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} intensity={0.5} />
+      <ambientLight intensity={0.5}  />
+      <directionalLight position={[0, 10, 0]} intensity={1} color={'#FFFFFF'} castShadow />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
       <Model animate={animate} />
       <OrbitControls />
     </Canvas>
   );
 }
+
