b1835a12bd5f0c170f646e29c27c0e964e8de4d0
Ian Skelskey <ianskelskey@gmail.com>
2023-09-25 20:29:56 -0400

    Update styling with tailwind

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import CubeCanvas from './three/Cube';
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center bg-primary text-text">
       <header className="w-full">
-        <h1 className="text-center text-3xl">Cody Sidlik</h1>
+        <h1 className="text-center text-3xl text-accent1">Cody Sidlik</h1>
       </header>
-      <main className="w-full flex">
-        <div className="w-1/2">
+      <main className="w-full flex bg-secondary">
+        <div className="w-1/2 p-4">
           {/* Code or specs go here */}
-          <pre className="font-mono">
+          <pre className="font-mono text-accent2">
             {`
               Material: Aluminum
               Dimensions: 10x10x5 cm
@@ -19,12 +19,12 @@ function App() {
             `}
           </pre>
         </div>
-        <div className="w-1/2">
+        <div className="w-1/2 p-4">
           {/* 3D model goes here */}
           <CubeCanvas />
         </div>
       </main>
-      <footer className="w-full">
+      <footer className="w-full text-center p-4 bg-accent2">
         {/* Hiring and pricing info go here */}
         <p>Contact us for hiring and pricing details.</p>
       </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#0D1117',
+        'secondary': '#161B22',
+        'accent1': '#58A6FF',
+        'accent2': '#F0F6FC',
+        'text': '#C9D1D9'
+      },
+    },
   },
   plugins: [],
 }
-
