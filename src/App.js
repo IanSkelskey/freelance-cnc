f81bd9f3a3fadead8d320a618abb309d0fed0e00
Ian Skelskey <ianskelskey@gmail.com>
2023-09-27 01:07:28 -0400

    Create light debug cubes also made superficial changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import React, { useState } from 'react';
 import ModelViewer from './three/ModelViewer';
 
 function App() {
-  const [animate, setAnimate] = useState(true);
+  const [animate, setAnimate] = useState(false);
 
   function PlayPauseButton() {
     return (
-      <button className='absolute top-5 left-5 p-4 bg-accent1 rounded-md text-white' onClick={() => setAnimate(!animate)}>{(animate)?'Stop':'Play'}</button>
+      <button className='absolute top-5 left-5 p-4 bg-accent1 rounded-md text-white' onClick={() => setAnimate(!animate)}>{(animate)?<PauseIcon color="#ffffff" />:<PlayIcon color="#ffffff" />}</button>
     );
   }
 
   return (
-    <div className='w-full h-screen flex flex-col items-center justify-center'>
+    <div className='w-full h-screen flex flex-col items-center justify-center bg-gradient-to-tr from-primary to-secondary'>
       <ModelViewer filename='test.obj' animate={animate} />
+      <div className='absolute top-5'>
+        <h1 className='text-5xl font-bold text-accent1'>Freelance CNC</h1>
+      </div>
       <PlayPauseButton />
     </div>
 
@@ -21,3 +24,20 @@ function App() {
 }
 
 export default App;
+
+// Flat play icon 
+function PlayIcon({color}) {
+  return (
+          <svg className='w-8 h-8' viewBox='0 0 24 24'>
+          <path fill={color} d='M8 5v14l11-7z' />
+          </svg>
+    );
+}
+
+function PauseIcon({color}) {
+  return (
+          <svg className='w-8 h-8' viewBox='0 0 24 24'>
+          <path fill={color} d='M6 19h4V5H6v14zm8-14v14h4V5h-4z' />
+          </svg>
+    );
+}
--- src/three/ModelViewer.js
+++ src/three/ModelViewer.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useMemo } from 'react';
 import { Canvas, useLoader, useFrame } from '@react-three/fiber';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { OrbitControls } from '@react-three/drei';
@@ -10,22 +10,24 @@ const InnerModel = ({ filename, animate }) => {
   const obj = useLoader(OBJLoader, modelDirectory + filename);
   const meshRef = useRef();
 
+  const material = useMemo(() => new MeshStandardMaterial({
+    color: '#AAAAAA',
+    dithering: true,
+    metalness: 0.82,
+    roughness: 0.2,
+  }), []);
+
   useFrame(() => {
-    if (animate) {
+    if (animate && meshRef.current) {
       meshRef.current.rotation.x += 0.005;
       meshRef.current.rotation.y += 0.005;
       meshRef.current.rotation.z += 0.005;
     }
   });
 
-  // Traverse the object and update the material
   obj.traverse((child) => {
     if (child instanceof Mesh) {
-      child.material = new MeshStandardMaterial({
-        color: '#ffffff',
-        metalness: 0.8,
-        roughness: 0.2,
-      });
+      child.material = material;
       child.scale.set(0.02, 0.02, 0.02);
     }
   });
@@ -33,13 +35,24 @@ const InnerModel = ({ filename, animate }) => {
   return <primitive object={obj} ref={meshRef} />;
 };
 
+const SmallCube = ({ position }) => {
+  return (
+    <mesh position={[...position]} receiveShadow>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'#0000FF'} />
+    </mesh>
+  );
+};
+
 const ModelViewer = ({ filename, animate }) => {
   return (
     <Canvas>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 10, 0]} intensity={1} color={'#FFFFFF'} castShadow />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <ambientLight intensity={1.5} />
+      <directionalLight position={[10, 12, 0]} intensity={1} color={'#CCCCFF'} castShadow/>
+      <directionalLight position={[-10, -12, 0]} intensity={1} color={'#FFFFFF'} castShadow />
       <InnerModel filename={filename} animate={animate} />
+      <SmallCube position={[10, 12, 0]} />
+      <SmallCube position={[-10, -12, 0]} />
       <OrbitControls />
     </Canvas>
   );
